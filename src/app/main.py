6b092e1cc7697fc96ce930099cab97d414a6bdaf
Guilherme Albuquerque <guilherme.hlalbuquerque@gmail.com>
2025-01-25 11:15:04 -0300

    fix: Fixed imports to deploy

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,8 @@
 """Main application module for the Smart Buyer Assistant."""
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 import streamlit as st
 import numpy as np
 import pandas as pd
